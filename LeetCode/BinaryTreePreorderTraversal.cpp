de5ca2bd659cf66d2771d6b9f99d4864c0778abb
Dilsekh Patnaik <dpj020@nist.edu>
2023-01-09 13:44:49 +0530

    09-01-23 LeetCode

---
+++ LeetCode/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,26 @@
+// Problem Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+// Leetcode I'd: https://leetcode.com/dilsekhpatnaik7/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root != NULL) {
+            ans.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+        return ans;
+    }
+};
