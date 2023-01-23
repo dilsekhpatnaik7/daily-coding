f2f7fc16cb94ec971f88d1e67772b0812c2edeaf
Dilsekh Patnaik <dpj020@nist.edu>
2023-01-23 14:41:56 +0530

    1.CP

---
+++ Striver's CP Sheet/Bit++.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://codeforces.com/problemset/problem/282/A?csrf_token=1f1c5990122e49d93478a8931d050c19
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int n, ans = 0;
+    string s;
+    cin >> n;
+    while(n--){
+        cin >> s;
+        if(s == "++X" || s == "X++") ans++;
+        if(s == "--X" || s == "X--") ans--
+    }
+    cout << ans;
+
+    return 0;
+}
