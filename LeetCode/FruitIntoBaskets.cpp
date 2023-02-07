a301ad70ce6b5885730b64ff0e7c1ae446de9840
Dilsekh Patnaik <dpj020@nist.edu>
2023-02-07 23:39:40 +0530

    07-02-23 LeetCode

---
+++ LeetCode/FruitIntoBaskets.cpp
@@ -0,0 +1,18 @@
+// Problem Link: https://leetcode.com/problems/fruit-into-baskets/
+// Leetcode I'd: https://leetcode.com/dilsekhpatnaik7/
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> count;
+        int i, j;
+        for (i = 0, j = 0; j < fruits.size(); ++j) {
+            count[fruits[j]]++;
+            if (count.size() > 2) {
+                if (--count[fruits[i]] == 0) count.erase(fruits[i]);
+                i++;
+            }
+        }
+        return j - i;
+    }
+};
