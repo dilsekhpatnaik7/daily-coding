13e00e81da9b8f5dccd1167f76d0be9f801f7dd7
Dilsekh Patnaik <dpj020@nist.edu>
2023-02-23 23:22:46 +0530

    23-02-23 LeetCode

---
+++ LeetCode/IPO.cpp
@@ -0,0 +1,27 @@
+// Problem Link: https://leetcode.com/problems/ipo/
+// Leetcode I'd: https://leetcode.com/dilsekhpatnaik7/
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> projects(n);
+        for (int i = 0; i < n; i++) {
+            projects[i] = {capital[i], profits[i]};
+        }
+        sort(projects.begin(), projects.end());
+        int i = 0;
+        priority_queue<int> maximizeCapital;
+        while (k--) {
+            while (i < n && projects[i].first <= w) {
+                maximizeCapital.push(projects[i].second);
+                i++;
+            }
+            if (maximizeCapital.empty())
+                break;
+            w += maximizeCapital.top();
+            maximizeCapital.pop();
+        }
+        return w;
+    }
+};
