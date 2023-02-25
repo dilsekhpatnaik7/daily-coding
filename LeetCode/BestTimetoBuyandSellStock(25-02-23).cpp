74f9e3fbeddb41a888dbd2be3b436350ca52f0e9
Dilsekh Patnaik <dpj020@nist.edu>
2023-02-25 15:05:58 +0530

    25-02-23 LeetCode

---
+++ LeetCode/BestTimetoBuyandSellStock(25-02-23).cpp
@@ -0,0 +1,14 @@
+// Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// Leetcode I'd: https://leetcode.com/dilsekhpatnaik7/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = INT_MAX, sell = INT_MIN;
+        for(int i = 0; i < prices.size(); i++){
+            buy = min(buy, prices[i]);
+            sell = max(sell, prices[i] - buy);
+        }
+        return sell > 0 ? sell : 0;
+    }
+};
