68d0a72974371790db67a6c550e1e0b1b56c64d9
Dilsekh Patnaik <dpj020@nist.edu>
2023-03-08 20:30:51 +0530

    08-03-23 LeetCode

---
+++ LeetCode/KokoEatingBananas.cpp
@@ -0,0 +1,25 @@
+// Problem Link: https://leetcode.com/problems/koko-eating-bananas/
+// Leetcode I'd: https://leetcode.com/dilsekhpatnaik7/
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1, right = 1e9;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canEatAll(piles, h, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    bool canEatAll(const vector<int>& piles, int h, int k) {
+        int hours = 0;
+        for (int pile : piles) {
+            hours += (pile + k - 1) / k;
+        }
+        return hours <= h;
+    }
+};
