6c777987616facd1c264bd4c855e6b853c17b1ba
Dilsekh Patnaik <dpj020@nist.edu>
2023-03-12 23:46:00 +0530

    12-03-23 LeetCode

---
+++ LeetCode/MergekSortedLists.cpp
@@ -0,0 +1,32 @@
+// Problem Link: https://leetcode.com/problems/merge-k-sorted-lists/
+// Leetcode I'd: https://leetcode.com/dilsekhpatnaik7/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return NULL;
+        ListNode* head = lists[0];
+        for(int i = 1; i < lists.size(); i++){
+            head = merge2Lists(head, lists[i]);
+        }
+        return head;
+    }
+};
