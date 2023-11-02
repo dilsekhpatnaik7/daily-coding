f6325526419ebef27fde9b3ca208ea593aa709b3
Dilsekh Patnaik <dpj020@nist.edu>
2023-11-02 23:15:50 +0530

    Step 16.4.8

---
+++ Striver A2Z DSA Sheet/Step 16/TargetSum-GFG.cpp
@@ -0,0 +1,38 @@
+/*
+Problem Link: https://practice.geeksforgeeks.org/problems/target-sum-1626326450/1
+
+Time Complexity: O(N*Target + N)
+Space Complexity: O(N*Target)
+*/
+
+class Solution {
+  public:
+    int solve(int index, int target, vector<int>& arr, vector<vector<int>>& dp) {
+        if(index == 0) {
+            if(target == 0 && arr[0] == 0) return 2;
+            if(target == 0 || target == arr[0]) return 1;
+            return 0;
+        }
+        if(dp[index][target] != -1) return dp[index][target];
+
+        int nonTake = solve(index - 1, target, arr, dp);
+        int take = 0;
+        if(arr[index] <= target) take = solve(index - 1, target - arr[index], arr, dp);
+
+        return dp[index][target] = (take + nonTake);
+    }
+
+    int countPartitions(int n, int d, vector<int> &arr) {
+        int totalSum = 0;
+        for(int i = 0; i < n; i++) totalSum += arr[i];
+        if(totalSum - d < 0 || (totalSum - d) % 2) return 0;
+
+        int target = (totalSum - d) / 2;
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        return solve(n - 1, target, arr, dp);
+    }
+    
+    int findTargetSumWays(vector<int>&A ,int target) {
+        return countPartitions(A.size(), target, A);
+    }
+};
