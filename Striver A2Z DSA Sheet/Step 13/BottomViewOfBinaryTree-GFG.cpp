67a16694738fc06d3a8ec3e8c5f3727173e8f9f1
Dilsekh Patnaik <dpj020@nist.edu>
2023-11-11 12:02:19 +0530

    Step 13.2.10

---
+++ Striver A2Z DSA Sheet/Step 13/BottomViewOfBinaryTree-GFG.cpp
@@ -0,0 +1,38 @@
+/*
+Problem Link: https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+*/
+
+class Solution {
+  public:
+    void solve(Node* root, vector<int>& ans) {
+        map<int, int> m;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+        
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            
+            Node* node = it.first;
+            int line = it.second;
+            m[line] = node->data;
+            
+            if(node->left) q.push({node->left, line - 1});
+            if(node->right) q.push({node->right, line + 1});
+        }
+        for(auto i: m) {
+            ans.push_back(i.second);
+        }
+    }
+  
+    vector <int> bottomView(Node *root) {
+        if(!root) return {};
+        
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
