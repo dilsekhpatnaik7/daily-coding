3fa66dbff1968e8ada3f11e0871304869269221e
Dilsekh Patnaik <dpj020@nist.edu>
2023-11-17 11:21:43 +0530

    Step 6.2.2

---
+++ Striver A2Z DSA Sheet/Step 6/InsertatendofDoublyLinkedList.cpp
@@ -0,0 +1,34 @@
+/*
+Problem Link: https://www.codingninjas.com/studio/problems/insert-at-end-of-doubly-linked-list_8160464?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+*/
+
+/**
+ * Definition of doubly linked list:
+ *
+ * struct Node {
+ *      int value;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() : value(0), prev(nullptr), next(nullptr) {}
+ *      Node(int val) : value(val), prev(nullptr), next(nullptr) {}
+ *      Node(int val, Node *p, Node *n) : value(val), prev(p), next(n) {}
+ * };
+ *
+ *************************************************************************/
+
+Node* insertAtTail(Node *head, int k) {
+    if(!head) return new Node(k);
+
+    Node* current = head;
+    while(current->next) {
+        current = current->next;
+    }
+    Node* newNode = new Node(k);
+    current->next = newNode;
+    newNode->prev = current;
+
+    return head;
+}
